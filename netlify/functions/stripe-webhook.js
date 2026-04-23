0bccc32a4a458f9849af6627cf446832f7f36ef1
limyxdi-ctrl <limyxdi@gmail.com>
2026-04-23 19:25:14 +0200

    Update stripe-webhook.js

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -9,11 +9,15 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (event) => {
   const sig = event.headers["stripe-signature"];
 
+  const rawBody = event.isBase64Encoded
+    ? Buffer.from(event.body, "base64").toString("utf8")
+    : event.body;
+
   let stripeEvent;
 
   try {
     stripeEvent = stripe.webhooks.constructEvent(
-      event.body,
+      rawBody,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET
     );
